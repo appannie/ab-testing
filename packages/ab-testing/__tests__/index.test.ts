25a0c5641f8d94e2334e5a4a3ad7930623a51a04
BananaWanted <i@BananaWanted.com>
2020-02-06 19:23:02 +0800

    impl for ab testing cohort matching

---
+++ packages/ab-testing/__tests__/index.test.ts
@@ -0,0 +1,131 @@
+/* eslint-disable @typescript-eslint/camelcase */
+import { getCohort, ABTestingConfig } from '../src';
+
+describe('ab-testing module', () => {
+    const config: ABTestingConfig = {
+        version: '1.0',
+        experiments: [
+            {
+                name: 'experiment_1',
+                cohorts: [
+                    { name: 'control', force_include: {} },
+                    {
+                        name: 'test_allocation',
+                        allocation: [
+                            [0, 10],
+                            [90, 100],
+                        ],
+                        force_include: {},
+                    },
+                    {
+                        name: 'test_force_include',
+                        force_include: {
+                            '559585298de65441d096a03315c848e25a5ceff9eea48bf5041e24ea4d481022': [
+                                '6b1bb552ddd9efa8188da1366386d303543a6de703d5ba8415f7a00b60e1eaea',
+                            ],
+                            '6ac346cad30f3196adbe9cb6f546cb8115b9f8998d1346b004481d1a6eb102f9': [
+                                '7ff9a510c4f06d3e30e21308ccf043d909f683e9b573929215c05c3e6f852c00',
+                            ],
+                            '0a107badb326491ebb2a4483d5b1d86e87c98ea21a34c29c32bf6205d0245e6e': [
+                                '2b474d6af1a14715371d450aedce427cfff78f6b9d2ceec5bf1321169206e046',
+                            ],
+                            '7aa814f6be9d13c1fc70d8d32a34dcce812381be4754c711540310b256e10e80': [
+                                '2c12caae54b0922eeba0560087d59d6a012eb431146e761c97787a28ca907625',
+                            ],
+                        },
+                    },
+                ],
+            },
+            {
+                name: 'experiment_2',
+                cohorts: [
+                    { name: 'control', force_include: {} },
+                    {
+                        name: 'test_allocation',
+                        allocation: [
+                            [10, 20],
+                            [80, 90],
+                        ],
+                        force_include: {},
+                    },
+                    {
+                        name: 'test_force_include',
+                        force_include: {
+                            '559585298de65441d096a03315c848e25a5ceff9eea48bf5041e24ea4d481022': [
+                                'd35efdfffec2f18661a2fdce592ba8237c8f12753bfaf3f3e7456304393764b8',
+                            ],
+                            '6ac346cad30f3196adbe9cb6f546cb8115b9f8998d1346b004481d1a6eb102f9': [
+                                '26e0a9e7e1dfe33ce15ef16cb7a1e0086ee7916ec10eb52a4a8fffe00ae3a40c',
+                            ],
+                            '0a107badb326491ebb2a4483d5b1d86e87c98ea21a34c29c32bf6205d0245e6e': [
+                                'ea799fdde1713dbf6bb5c666c3117ab8d19254966997fa413394c0b33ca5bab6',
+                            ],
+                            '7aa814f6be9d13c1fc70d8d32a34dcce812381be4754c711540310b256e10e80': [
+                                '10a3c3b449ed70682d756f3f6b980595ebc284819250730a4ffc184e08c0b3f6',
+                            ],
+                        },
+                    },
+                ],
+            },
+        ],
+        salt: '4a9120a277117afeade34305c258a2f1',
+    };
+
+    it('match cohorts', () => {
+        expect(getCohort(config, 'experiment_1', { user_id: 2 })).toEqual('control');
+        expect(getCohort(config, 'experiment_1', { user_id: 1 })).toEqual('test_force_include');
+        expect(getCohort(config, 'experiment_1', { user_id: 2, user_type: 'free' })).toEqual(
+            'test_force_include'
+        );
+        expect(
+            getCohort(config, 'experiment_1', { user_id: 2, user_type: 'intelligence' })
+        ).toEqual('control');
+        expect(
+            getCohort(config, 'experiment_1', { user_id: 2, email: 'control@example.com' })
+        ).toEqual('test_force_include');
+        expect(getCohort(config, 'experiment_1', { user_id: 2, email: 'control@a.com' })).toEqual(
+            'control'
+        );
+        expect(
+            getCohort(config, 'experiment_1', { user_id: 2, email_domain: 'example.com' })
+        ).toEqual('test_force_include');
+        expect(getCohort(config, 'experiment_1', { user_id: 2, email_domain: 'a.com' })).toEqual(
+            'control'
+        );
+        expect(
+            Array(100)
+                .fill(0)
+                .map((_, i) => getCohort(config, 'experiment_1', { user_id: i + 3 }))
+        ).toMatchSnapshot();
+
+        expect(getCohort(config, 'experiment_2', { user_id: 1 })).toEqual('control');
+        expect(getCohort(config, 'experiment_2', { user_id: 2 })).toEqual('test_force_include');
+        expect(getCohort(config, 'experiment_2', { user_id: 1, user_type: 'free' })).toEqual(
+            'control'
+        );
+        expect(
+            getCohort(config, 'experiment_2', { user_id: 1, user_type: 'intelligence' })
+        ).toEqual('test_force_include');
+        expect(
+            getCohort(config, 'experiment_2', { user_id: 1, email: 'control@example.com' })
+        ).toEqual('control');
+        expect(getCohort(config, 'experiment_2', { user_id: 1, email: 'control@a.com' })).toEqual(
+            'test_force_include'
+        );
+        expect(
+            getCohort(config, 'experiment_2', { user_id: 1, email_domain: 'example.com' })
+        ).toEqual('control');
+        expect(getCohort(config, 'experiment_2', { user_id: 1, email_domain: 'a.com' })).toEqual(
+            'test_force_include'
+        );
+        expect(
+            Array(100)
+                .fill(0)
+                .map((_, i) => getCohort(config, 'experiment_2', { user_id: i + 3 }))
+        ).toMatchSnapshot();
+
+        expect(
+            getCohort(config, 'experiment_3', { user_id: 1, email_domain: 'example.com' })
+        ).toEqual('control');
+    });
+});
--- packages/ab-testing/src/index.ts
+++ packages/ab-testing/src/index.ts
@@ -1,9 +1,70 @@
-// add another package with `yo lerna-typescript:package @myscope/my-new-package` instead of `lerna create`
+import createHash from 'create-hash';
+import crc32 from 'fast-crc32c';
 
-export function greet(name: string): string {
-    return `ab-testing says: hello to ${name}`;
+interface ForceInclude {
+    [hashedKey: string]: string[];
 }
 
-export function add(a: number, b: number): number {
-    return a + b;
+interface Cohort {
+    name: string;
+    allocation?: [number, number][];
+    force_include?: ForceInclude;
+}
+
+interface Experiment {
+    name: string;
+    cohorts: Cohort[];
+}
+
+export interface ABTestingConfig {
+    salt: string;
+    version: '1.0';
+    experiments: Experiment[];
+}
+
+interface UserProfile {
+    user_id: number;
+    user_type?: string;
+    email?: string;
+    email_domain?: string;
+}
+
+function getHashedValue(salt: string, sourceValue: unknown): string {
+    const hash = createHash('sha256');
+    hash.update(salt);
+    hash.update(String(sourceValue));
+    return hash.digest('hex');
+}
+
+function getModuloValue(experiment: string, userId: number): number {
+    return crc32.calculate(String(userId), crc32.calculate(experiment)) % 100;
+}
+
+export function getCohort(
+    config: ABTestingConfig,
+    experimentName: string,
+    profile: UserProfile
+): string {
+    const experimentConfig = config.experiments.find(e => e.name === experimentName);
+    for (const cohort of experimentConfig?.cohorts || []) {
+        for (const profileKey of Object.keys(profile) as Iterable<keyof UserProfile>) {
+            const hashedProfileKey = getHashedValue(config.salt, profileKey);
+            const hashedProfileVal = getHashedValue(config.salt, profile[profileKey]);
+            if (
+                cohort.force_include &&
+                cohort.force_include[hashedProfileKey]?.includes(hashedProfileVal)
+            ) {
+                return cohort.name;
+            }
+        }
+    }
+    const userSegmentNum = getModuloValue(experimentName, profile.user_id);
+    for (const cohort of experimentConfig?.cohorts || []) {
+        for (const allocation of cohort.allocation || []) {
+            if (allocation[0] <= userSegmentNum && userSegmentNum < allocation[1]) {
+                return cohort.name;
+            }
+        }
+    }
+    return 'control';
 }

daacad8eae0deace71e711e3f088d64d17ae8c2c
BananaWanted <i@BananaWanted.com>
2020-02-10 18:30:15 +0800

    support for hash array object

--- packages/ab-testing-hash-object/src/index.ts
+++ packages/ab-testing-hash-object/src/index.ts
@@ -8,14 +8,28 @@ export function hashWithSalt(value: unknown, salt: string): string {
     return hash.digest('hex');
 }
 
+export function hashArray(values: unknown[], salt: string): string[] {
+    return values.map(val => hashWithSalt(val, salt));
+}
+
 export function hashObject(
     obj: { [key: string]: string | number | undefined },
     salt: string
-): { [s: string]: string } {
+): { [key: string]: string };
+
+export function hashObject(
+    obj: { [key: string]: string[] | number[] | undefined },
+    salt: string
+): { [key: string]: string[] };
+
+export function hashObject(obj: { [s: string]: unknown }, salt: string): unknown {
     return Object.fromEntries(
         Object.entries(obj)
             .filter(([, value]) => value !== undefined)
-            .map(([key, value]) => [hashWithSalt(key, salt), hashWithSalt(value, salt)])
+            .map(([key, value]) => [
+                hashWithSalt(key, salt),
+                Array.isArray(value) ? hashArray(value, salt) : hashWithSalt(value, salt),
+            ])
     );
 }
 

328c7484974f5af16136fd4f04c2e8289a34f981
BananaWanted <i@BananaWanted.com>
2020-02-19 19:19:02 +0800

    fix typedef

--- @types/fast-crc32c/index.d.ts
+++ @types/fast-crc32c/index.d.ts
@@ -1,3 +1,7 @@
 declare module 'fast-crc32c' {
     export function calculate(data: string | Buffer, initial?: number): number;
 }
+
+declare module 'fast-crc32c/impls/js_crc32c' {
+    export * from 'fast-crc32c';
+}

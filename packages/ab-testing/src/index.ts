ea4c1605744ea12523da5c6c7c181af03daf4d5f
Ramon Villain <ramon.villain@gmail.com>
2020-11-09 13:22:53 -0800

    Don't console.error if NODE_ENV is test

--- packages/ab-testing/src/index.ts
+++ packages/ab-testing/src/index.ts
@@ -71,7 +71,8 @@ export class Experiments {
         if (!(experimentName in this.matchedCohorts)) {
             const experimentConfig: Experiment = this.config[experimentName];
             if (experimentConfig == null) {
-                console.error(`unrecognized ab testing experiment name: ${experimentName}`);
+                !process.env.NODE_ENV &&
+                    console.error(`unrecognized ab testing experiment name: ${experimentName}`);
                 this.matchedCohorts[experimentName] = 'control';
             } else {
                 this.matchedCohorts[experimentName] = matchUserCohort(

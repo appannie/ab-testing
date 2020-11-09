c22079e31231942155a85f8c483d5be79d6aae60
Ramon Villain <ramon.villain@gmail.com>
2020-11-09 13:23:36 -0800

    typo

--- packages/ab-testing/src/index.ts
+++ packages/ab-testing/src/index.ts
@@ -71,7 +71,7 @@ export class Experiments {
         if (!(experimentName in this.matchedCohorts)) {
             const experimentConfig: Experiment = this.config[experimentName];
             if (experimentConfig == null) {
-                !process.env.NODE_ENV &&
+                process.env.NODE_ENV !== 'test' &&
                     console.error(`unrecognized ab testing experiment name: ${experimentName}`);
                 this.matchedCohorts[experimentName] = 'control';
             } else {
